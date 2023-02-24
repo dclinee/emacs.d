affbe458a1becf158174672ff2ca5d055ce5fda3
dclinee <dengchanglin8@outlook.com>
2023-02-24 20:44:49 +0800

    Modified init-avy.el and init.el files.

--- lisp/init-avy.el
+++ lisp/init-avy.el
@@ -6,6 +6,7 @@
 (global-set-key (kbd "M-C") 'avy-goto-char-2)
 (global-set-key (kbd "M-g w") 'avy-goto-word-1)
 (global-set-key (kbd "M-g e") 'avy-goto-word-0)
-(global-set-key (kbd "M-g f") 'avy-goto-line)
+(global-set-key (kbd "M-g g") 'avy-goto-line)
+
 (provide 'init-avy)
 ;;; init-avy.el ends here
