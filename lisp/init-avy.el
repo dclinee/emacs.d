91c7592a95c2dfd7cd35ea792c269183a57c8e33
dclinee <dengchanglin8@outlook.com>
2025-01-30 16:43:51 +0800

    Modifed init-avy.el about adding the function: avy-goto-conditional.

--- lisp/init-avy.el
+++ lisp/init-avy.el
@@ -7,13 +7,21 @@
 
 
 (global-set-key (kbd "M-g ;") #'avy-goto-char-timer)
-(global-set-key (kbd "M-g c") #'avy-goto-char)
+(global-set-key (kbd "C-:") #'avy-goto-char)
 (global-set-key (kbd "M-g v") #'avy-goto-char-2)
 (global-set-key (kbd "M-g w") #'avy-goto-word-1)
 (global-set-key (kbd "M-g e") #'avy-goto-word-0)
 (global-set-key (kbd "M-g g") #'avy-goto-line)
 (global-set-key (kbd "M-g l") #'avy-copy-line)
 (global-set-key (kbd "M-g r") #'avy-copy-region)
+;;
+;;;Jumping to conditionals in Elisp
+(defun avy-goto-conditional ()
+"Jump to conditional keywords like 'if', 'cond', 'when', 'unless' in the text."
+  (interactive)
+  (avy--generic-jump "\\s(\\(if\\|cond\\|when\\|unless\\)\\b" nil 0))
+(global-set-key (kbd "M-g c") 'avy-goto-conditional)
+;;
 
 
 (provide 'init-avy)
