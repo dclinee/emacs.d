82336743ef4f89584a7cb2be06b09b1fd734a9a1
dclinee <dengchanglin8@outlook.com>
2025-02-15 22:05:16 +0800

    Modifed init-helm.el file.

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -4,22 +4,23 @@
 
 (use-package helm
   :ensure t
+  :defer t
   :bind (("M-x" . helm-M-x)
          ("C-x b" . helm-mini)
          ("M-y" . helm-show-kill-ring)
          ("C-x C-f" . helm-find-files))
   :config
   (helm-mode t)
-  (helm-autoresize-mode t))
+  (helm-autoresize-mode t)
+  (setq helm-move-to-line-cycle-in-source nil)
+  (setq helm-M-x-fuzzy-match t)
+  (setq helm-buffers-fuzzy-match t
+        helm-recentf-fuzzy-match t))
 
 
 (global-set-key (kbd "C-c h") #'helm-command-prefix)
 (global-unset-key (kbd "C-x c"))
 
-(setq helm-move-to-line-cycle-in-source nil)
-(setq helm-M-x-fuzzy-match t)
-(setq helm-buffers-fuzzy-match t
-      helm-recentf-fuzzy-match t)
 
 (provide 'init-helm)
 ;;; init-helm.el ends here
